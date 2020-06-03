9d8d6054fa1268884bb77644442fc27ff31ff802
David Quinn <david.c.quinn25@gmail.com>
2020-06-03 14:49:43 -0700

    got initial inde.html working

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// car knowsManBearPig = prompt ('do you know Man Bear Pig');
+
+// if(knowsManBearPig === 'y' || knowsManBearPig === 'yep'){
+//   alert('styling must be easy for you');
+// } else if(knowsManBearPig === 'n'){
+//   alert('man bear pig is how border margin and padding work');
+// }else(
+//   knowsManBearPig = prompt('you must talk to me about man bear pig - y or yep on n please');
+// }
+
+// pick a while loop - go until the condition is false - go until they answer
+// todo do while
+// while(knowsManBearPig !== 'y' && knowsManBearPig !== 'n' && knowsManBearPig !== 'yep'){ 
+// }
+
+
+
+
+// var firstArray = ['man', 'bear', 'pig'];
+// array start at index
+
+// alert( firstArray[1]);
+
+// for(var index = 0; index < 3; index++){
+//   alert(firstArray[index]);
+// }
+// }
+
+// for(var i - 0; i < firstArray.length; i++){
+//   alert(firstArray);
+// }
