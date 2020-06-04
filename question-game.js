53436af446a5a1b234382ac37681c4b5917e5ea3
Chandler Puckett <chandler.puckett1@gmail.com>
2020-06-04 15:12:05 -0700

    working functions

--- question-game.js
+++ question-game.js
@@ -1,99 +1,115 @@
 'use strict';
 var username = prompt('hello what is your name');
-alert('Hello ' + username + 'Welcome!')
+alert('Hello ' + username + 'Welcome!');
 //
 
-function lastName(){
-  var lastName = prompt('What is David\'s last name');
-  if (lastName === 'Quinn' || lastName ==='quinn'){
-    alert('Nice Job');
-  } else {
-    alert('no it\'s Quinn.');
-  }
-}
+// function lastName(){
+//   var lastName = prompt('What is David\'s last name');
+//   if (lastName === 'Quinn' || lastName ==='quinn'){
+//     alert('Nice Job');
+//   } else {
+//     alert('no it\'s Quinn.');
+//   }
+// }
 
-lastName();
+// lastName();
 
-function colorQuestion(){
-  var favoriteColor = prompt('what David\'s favorite color? Hint its the color of his eyes sometimes');
-  if (favoriteColor !== 'blue')
-  // console(log)
-    favoriteColor = alert('wrong its Blue');
-  if (favoriteColor === 'blue')
-  // console(log)
-    alert('Nice Job');
-}
+// function colorQuestion(){
+//   var favoriteColor = prompt('what David\'s favorite color? Hint its the color of his eyes sometimes');
+//   if (favoriteColor !== 'blue')
+//   // console(log)
+//     favoriteColor = alert('wrong its Blue');
+//   if (favoriteColor === 'blue')
+//   // console(log)
+//     alert('Nice Job');
+// }
 
-colorQuestion();
+// colorQuestion();
 
-function australiaQuestion(){
-  var australiaAnswer = prompt('has David been to Australia?');
-  if (australiaAnswer !== 'yes')
-  // console(log)
-    australiaAnswer = alert('actually he has its beautiful there');
-  if (australiaAnswer === 'yes')
-  // console(log)
-    alert('Nice Job');
-}
+// function australiaQuestion(){
+//   var australiaAnswer = prompt('has David been to Australia?');
+//   if (australiaAnswer !== 'yes')
+//   // console(log)
+//     australiaAnswer = alert('actually he has its beautiful there');
+//   if (australiaAnswer === 'yes')
+//   // console(log)
+//     alert('Nice Job');
+// }
 
-australiaQuestion();
+// australiaQuestion();
 
-function mexicoQuestion(){
-  var mexico = prompt('has David been to mexico?');
-  if (mexico !== 'yes');
-  // console(log)
-  mexico = alert('actually he has multiple times');
-  if (mexico === 'yes');
-  // console(log)
-  alert('Nice Job');
-}
+// function mexicoQuestion(){
+//   var mexico = prompt('has David been to mexico?');
+//   if (mexico !== 'yes');
+//   // console(log)
+//   mexico = alert('actually he has multiple times');
+//   if (mexico === 'yes');
+//   // console(log)
+//   alert('Nice Job');
+// }
 
-mexicoQuestion();
+// mexicoQuestion();
 
-function timesMexico(){
-  var timesBeenMexico = prompt('How many times has he been to mexico?');
-  if (timesBeenMexico === '4'){
-    alert('Nice Job');
-  } else {
-    alert('Wrong. he has been cuatro times');
-  }
-}
+// function timesMexico(){
+//   var timesBeenMexico = prompt('How many times has he been to mexico?');
+//   if (timesBeenMexico === '4'){
+//     alert('Nice Job');
+//   } else {
+//     alert('Wrong. he has been cuatro times');
+//   }
+// }
 
-timesMexico();
+// timesMexico();
 
-function ageQuestion(){
-  var ageQuestion = prompt('how old is david?');
-  if (ageQuestion === '24')
-    alert('Nice Job');
-  // console(log)
-  var count = 1;
-  while (ageQuestion != 24 && count<4){
-    if (ageQuestion < 24)
-      alert('Nope try a little higher');
-    if (ageQuestion > 24)
-      alert('Nope try a little lower');
-    // console(log)
-    ageQuestion = prompt('how old is david?');
-    count++;
-  }
-  alert("David is 24 (year of Kobe)");
-}
+// function ageQuestion(){
+//   var ageQuestion = prompt('how old is david?');
+//   if (ageQuestion === '24')
+//     alert('Nice Job');
+//   // console(log)
+//   var count = 1;
+//   while (ageQuestion != 24 && count<4){
+//     if (ageQuestion < 24)
+//       alert('Nope try a little higher');
+//     if (ageQuestion > 24)
+//       alert('Nope try a little lower');
+//     // console(log)
+//     ageQuestion = prompt('how old is david?');
+//     count++;
+//   }
+//   alert("David is 24 (year of Kobe)");
+// }
 
-ageQuestion();
+// ageQuestion();
 
 // for(var i = 0; i < firstArray.length; i++){
 //         alert(firstArray[i]);
 
 
-continent = prompt('What continent has David been to?');
-var myContinents = ['north america', 'europe', 'asia', 'australia'];
-var correctContinent = false;
+function continentQuestion(){
+  var myContinents = ['north america', 'europe', 'asia', 'australia'];
+  var correctContinent = false;
+  var count = 0;
 
-while (correctContinents == false && count<6)
-  for(var i =0; i < myContinents.length; i++){
-    if (continent === myContinents[i]) {alert('Good job!'); correctContinent = true;}
 
+  // runs once for every chance they have
+  while (correctContinent === false && count < 6){
+    var continent = prompt('What continent has David been to?');
+    count++;
+    // this loop verifys the answer
+    for(var i = 0; i < myContinents.length; i++){
+      if (continent === myContinents[i]){
+        alert('Good job!');
+        correct++;
+        correctContinent = true;
+      }
+    }
+    if (correctContinent === false){
+      alert('wrong');
+    }
   }
+}
+
+continentQuestion();
 
 var today = new Date();
 var hourNow = today.getHours();
@@ -113,5 +129,5 @@ document.write('<h3>'+ greeting + '</h3>');
 var username = prompt('what is your name');
 
 document.write('<h3>' + username + '</h3>');
-var parent = document.getElementById('username')
+var parent = document.getElementById('username');
 
