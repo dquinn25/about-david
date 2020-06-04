d427ea1a234d115e32887b7d33ad6d4ed262dc65
Chandler Puckett <chandler.puckett1@gmail.com>
2020-06-04 14:33:28 -0700

    CPDQ mexico times question

--- question-game.js
+++ question-game.js
@@ -14,102 +14,104 @@ function lastName(){
 
 lastName();
 
-// function colorQuestion(){
-//   var favoriteColor = prompt('what David\'s favorite color? Hint its the color of his eyes sometimes');
-//   if (favoriteColor !== 'blue')
-//   // console(log)
-//     favoriteColor = alert('wrong its Blue');
-//   if (favoriteColor === 'blue')
-//   // console(log)
-//     alert('Nice Job');
-// }
+function colorQuestion(){
+  var favoriteColor = prompt('what David\'s favorite color? Hint its the color of his eyes sometimes');
+  if (favoriteColor !== 'blue')
+  // console(log)
+    favoriteColor = alert('wrong its Blue');
+  if (favoriteColor === 'blue')
+  // console(log)
+    alert('Nice Job');
+}
 
-// colorQuestion();
+colorQuestion();
 
-// function australiaQuestion(){
-//   var australiaAnswer = prompt('has David been to Australia?');
-//   if (australiaAnswer !== 'yes')
-//   // console(log)
-//     australiaAnswer = alert('actually he has its beautiful there');
-//   if (australiaAnswer === 'yes')
-//   // console(log)
-//     alert('Nice Job');
-// }
+function australiaQuestion(){
+  var australiaAnswer = prompt('has David been to Australia?');
+  if (australiaAnswer !== 'yes')
+  // console(log)
+    australiaAnswer = alert('actually he has its beautiful there');
+  if (australiaAnswer === 'yes')
+  // console(log)
+    alert('Nice Job');
+}
 
-// australiaQuestion();
+australiaQuestion();
 
-// function mexicoQuestion(){
-//   var mexico = prompt('has David been to mexico?');
-//   if (mexico !== 'yes');
-//   // console(log)
-//   mexico = alert('actually he has multiple times');
-//   if (mexico === 'yes');
-//   // console(log)
-//   alert('Nice Job');
-// }
+function mexicoQuestion(){
+  var mexico = prompt('has David been to mexico?');
+  if (mexico !== 'yes');
+  // console(log)
+  mexico = alert('actually he has multiple times');
+  if (mexico === 'yes');
+  // console(log)
+  alert('Nice Job');
+}
 
-// mexicoQuestion();
+mexicoQuestion();
 
-// function timesMexico(){
-//   var timesBeenMexico = prompt('How many times has he been to mexico?');
-//   if (timesBeenMexico !== '4');
-//   // console(log)
-//   timesBeenMexico = alert('wrong he has been 4 times');
-//   if (timesBeenMexico === '4');
-//   // console(log)
-//   alert('Nice Job');
-// }
+function timesMexico(){
+  var timesBeenMexico = prompt('How many times has he been to mexico?');
+  if (timesBeenMexico === '4'){
+    alert('Nice Job');
+  } else {
+    alert('Wrong. he has been cuatro times');
+  }
+}
 
+timesMexico();
 
-// function ageQuestion(){
-//   var ageQuestion = prompt('how old is david?');
-//   if (ageQuestion === '24')
-//     alert('Nice Job');
-//   // console(log)
-//   var count = 1;
-//   while (ageQuestion != 24 && count<4){
-//     if (ageQuestion < 24)
-//       alert('Nope try a little higher');
-//     if (ageQuestion > 24)
-//       alert('Nope try a little lower');
-//     // console(log)
-//     ageQuestion = prompt('how old is david?');
-//     count++;
-//   }
-//   alert("David is 24 (year of Kobe)");
-// }
+function ageQuestion(){
+  var ageQuestion = prompt('how old is david?');
+  if (ageQuestion === '24')
+    alert('Nice Job');
+  // console(log)
+  var count = 1;
+  while (ageQuestion != 24 && count<4){
+    if (ageQuestion < 24)
+      alert('Nope try a little higher');
+    if (ageQuestion > 24)
+      alert('Nope try a little lower');
+    // console(log)
+    ageQuestion = prompt('how old is david?');
+    count++;
+  }
+  alert("David is 24 (year of Kobe)");
+}
 
-// // for(var i = 0; i < firstArray.length; i++){
-// //         alert(firstArray[i]);
+ageQuestion();
 
-
-// continent = prompt('What continent has David been to?');
-// var myContinents = ['north america', 'europe', 'asia', 'australia'];
-// var correctContinent = false;
-
-// while (correctContinents == false && count<6)
-//   for(var i =0; i < myContinents.length; i++){
-//     if (continent === myContinents[i]) {alert('Good job!'); correctContinent = true;}
-
-//   }
-
-// var today = new Date();
-// var hourNow = today.getHours();
-// var greeting;
-// if (hourNow > 18) {
-//   greeting = 'Good Evening!';
-// } else if (hourNow > 12) {
-//   greeting = 'Good Afternoon';
-// } else if (hourNow > 0) {
-//   greeting = 'Good Morning!';
-// } else {
-//   greeting = 'Welcome!';
-// }
-// document.write('<h3>'+ greeting + '</h3>');
+// for(var i = 0; i < firstArray.length; i++){
+//         alert(firstArray[i]);
 
 
-// var username = prompt('what is your name');
+continent = prompt('What continent has David been to?');
+var myContinents = ['north america', 'europe', 'asia', 'australia'];
+var correctContinent = false;
 
-// document.write('<h3>' + username + '</h3>');
-// var parent = document.getElementById('username')
+while (correctContinents == false && count<6)
+  for(var i =0; i < myContinents.length; i++){
+    if (continent === myContinents[i]) {alert('Good job!'); correctContinent = true;}
+
+  }
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+if (hourNow > 18) {
+  greeting = 'Good Evening!';
+} else if (hourNow > 12) {
+  greeting = 'Good Afternoon';
+} else if (hourNow > 0) {
+  greeting = 'Good Morning!';
+} else {
+  greeting = 'Welcome!';
+}
+document.write('<h3>'+ greeting + '</h3>');
+
+
+var username = prompt('what is your name');
+
+document.write('<h3>' + username + '</h3>');
+var parent = document.getElementById('username')
 
